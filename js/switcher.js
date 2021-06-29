6165c809c5ede301e52552b4ebe2f953cd9368c3
namnguyenthanhwork <78526193+namnguyenthanhwork@users.noreply.github.com>
2021-06-29 23:48:39 +0700

    update

---
+++ js/switcher.js
@@ -0,0 +1,27 @@
+/* --------------- toggle switcher ------------ */
+const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+
+styleSwitcherToggler.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+});
+
+// hide switcher on scroll
+window.addEventListener("scroll", () => {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
+        (document.querySelector(".style-switcher").classList.remove("open"));
+    }
+});
+
+/* --------------- toggle switcher color ------------ */
+const alternateStyles = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute("title"))
+            style.removeAttribute("disabled");
+        else
+            style.setAttribute("disabled", "true");
+    })
+}
+
+/* ---------------theme light - dark mode ------------ */
