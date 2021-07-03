ab863abd5aa229e4f9ebc634d0d7e63326bcae47
namnguyenthanhwork <78526193+namnguyenthanhwork@users.noreply.github.com>
2021-07-03 20:45:10 +0700

    update

--- js/particles.js
+++ js/particles.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
     "particles": {
         "number": {
-            "value": 80,
+            "value": 70,
             "density": {
                 "enable": true,
                 "value_area": 1000
@@ -36,7 +36,7 @@ particlesJS("particles-js", {
             }
         },
         "size": {
-            "value": 3,
+            "value": 4,
             "random": true,
             "anim": {
                 "enable": false,
